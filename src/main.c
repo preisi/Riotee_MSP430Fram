f534e7d4b97dae77209d6da42dcb9b292bcd63e9
Thomas <mail@tpreisner.de>
2026-02-10 12:16:33 +0100

    Modify printf statements for DMA command buffer logging
    Updated printf statements to log dma_cmd_buf instead of addr.

--- src/main.c
+++ src/main.c
@@ -172,14 +172,14 @@ static inline int setup_transfer() {
 
   /* Write request */
   if (dma_cmd_buf[2] & CMD_RW_WRITE_Msk) {
-    printf("w: %X, %X\r\n", addr, *addr);
+    printf("w: %X\r\n", dma_cmd_buf);
     _data16_write_addr(&DMA1DA, addr);
     UCA0IFG &= ~UCRXIFG;
     DMA1CTL |= DMAEN;
     DMA1SZ = transfer_size;
     /* Read request */
   } else {
-    printf("r: %X, %X\r\n", addr + 1, *addr);
+    printf("r: %X\r\n", dma_cmd_buf);
     UCA0TXBUF = *addr;
     _data16_write_addr(&DMA2SA, addr + 1);
     DMA2CTL |= DMAEN;
